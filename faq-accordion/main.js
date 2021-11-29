d766dd9110f028e2413ac4fb02107dfa58022e81
Olesia <okissaw@gmail.com>
2021-11-29 19:48:54 +0200

    Add accordion functionality and animations

---
+++ faq-accordion/main.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+  initAccordion();
+};
+
+function initAccordion() {
+  const btnCollapsibleArray = document.getElementsByClassName("collapsible");
+
+  for (let i = 0; i < btnCollapsibleArray.length; i++) {
+    btnCollapsibleArray[i].addEventListener("click", function () {
+      if (this.classList.contains("active")) {
+        collapseItems(btnCollapsibleArray);
+      } else {
+        collapseItems(btnCollapsibleArray);
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("visible");
+      }
+    });
+  }
+}
+
+function collapseItems(accordion) {
+  for (let i = 0; i < accordion.length; i++) {
+    accordion[i].classList.toggle("active", false);
+    accordion[i].nextElementSibling.classList.toggle("visible", false);
+  }
+}
