aee6350c829a64e4b035bade0d89c4c2d12d4b87
Olesia <okissaw@gmail.com>
2021-12-19 11:41:14 +0200

    Add 'sharing to socials' functionality

--- article-preview/main.js
+++ article-preview/main.js
@@ -1,20 +1,59 @@
 $(document).ready(function() {
   const modalShareBtn = $(".btn-share");
   const modalShareDialog = $(".modal-share");
-  console.log('document loaded');
-  
-  modalShareBtn.click(function(){
-    modalShareDialog
-    .fadeIn("fast")
-    .css("display", "flex")
-    .addClass("open");
+
+  const btnShareFacebook = $(".btn-facebook");
+  const btnShareTwitter = $(".btn-twitter");
+  const btnSharePinterest = $(".btn-pinterest");
+
+  const currentUrl = window.location.href;
+  const articleTitle = $(".article-title").text().trim();
+  const imageUrl = $(".article-img").attr("src");
+
+  modalShareBtn.click(function() {
+    modalShareDialog.fadeIn("fast").css("display", "flex").addClass("open");
   });
 
   modalShareDialog.click(function() {
-    $(this)
-      .fadeOut("fast", function() {
-        $(this).removeClass("open");
-      });
+      $(this).fadeOut("fast", function() {
+      $(this).removeClass("open");
+    });
   });
+
+  // Rendering share to socials
+  btnShareFacebook.click(function(e) {
+    e.stopPropagation();
+
+    shareToFacebook(currentUrl);
+  });
+
+  btnShareTwitter.click(function(e) {
+    e.stopPropagation();
+
+    shareToTwitter(currentUrl, articleTitle);
+  });
+
+  btnSharePinterest.click(function(e) {
+    e.stopPropagation();
+
+    shareToPinterest(currentUrl, articleTitle, imageUrl);
+  });
+
+  function shareToFacebook(url) {
+    let shareUrl = `http://www.facebook.com/sharer/sharer.php?u=${url}`;
+    window.open(shareUrl, "fb-popup", "");
+  }
+
+  function shareToTwitter(url) {
+    let shareUrl = `https://twitter.com/intent/tweet?url=${url}&text=${title}`;
+    window.open(shareUrl);
+  }
+
+  function shareToPinterest(url, title, imageUrl) {
+    let shareUrl = `http://pinterest.com/pin/create/button/?url=${url}
+                                                          &media=${imageUrl}   
+                                                          &description=${title}`;
+    window.open(shareUrl)
+  }
 
 });
