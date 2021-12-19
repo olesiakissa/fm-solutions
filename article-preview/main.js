af3c382a74b0e23a01057a93276106fdadfee180
Olesia <okissaw@gmail.com>
2021-12-19 15:25:01 +0200

    Add article desktop layout

--- article-preview/main.js
+++ article-preview/main.js
@@ -12,23 +12,44 @@ $(document).ready(function() {
   const imageUrl = $(".article-img").attr("src");
 
   modalShareOpenBtn.click(function() {
+    // Additional check for the desktop version that 
+    // checks if the button has already been clicked (turned white with dark bg)
+    if (!$(this).hasClass("colorize-white")) {
+      if (window.innerWidth >= 1440) {
+        const shareButtonHeight = modalShareOpenBtn.height();
+        const modalHeight = modalShareDialog.height();
+
+        modalShareDialog.css("bottom", `${(modalHeight + shareButtonHeight) + 50}px`);
+        $(this).toggleClass("btn-modal-share colorize-white");
+    }
     modalShareDialog.fadeIn("fast").css("display", "flex").addClass("open");
     // We have to make this button invisible for screen reader because
     // it keeps reading it when the share dialog is opened
-    $(this).attr("aria-hidden", "true");
+    if (window.innerWidth < 1440) {
+      $(this).attr("aria-hidden", "true");
+    }
     // Moving focus to the buttons in modal
     btnShareFacebook.focus();
+    } else {
+      $(this).removeClass("btn-modal-share colorize-white");
+      closeModal();
+    }
+
   });
 
-  modalShareDialog.click(function() {
-      $(this).fadeOut("fast", function() {
-      $(this).removeClass("open");
+  function closeModal() {
+    modalShareDialog.fadeOut("fast", function() {
+      modalShareDialog.removeClass("open");
     });
-  });
+    modalShareOpenBtn.delay(200).removeClass("btn-modal-share colorize-white");
+  }
+
+  modalShareDialog.click(closeModal);
 
   modalShareCloseBtn.click(function() {
     modalShareOpenBtn.attr("aria-hidden", "false");
   });
+
 
   // Rendering share to socials
   btnShareFacebook.click(function(e) {
