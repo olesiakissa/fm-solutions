075318154b22708114face77dbd8af47e767ed09
Olesia <okissaw@gmail.com>
2021-12-20 11:50:45 +0200

    Add URL encoding

--- article-preview/main.js
+++ article-preview/main.js
@@ -70,20 +70,21 @@ $(document).ready(function() {
     shareToPinterest(currentUrl, articleTitle, imageUrl);
   });
 
+
   function shareToFacebook(url) {
-    let shareUrl = `http://www.facebook.com/sharer/sharer.php?u=${url}`;
-    window.open(shareUrl, "fb-popup", "");
+    let shareUrl = `http://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+    window.open(shareUrl);
   }
 
   function shareToTwitter(url, title) {
-    let shareUrl = `https://twitter.com/intent/tweet?url=${url}&text=${title}`;
+    let shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${title}`;
     window.open(shareUrl);
   }
 
   function shareToPinterest(url, title, imageUrl) {
-    let shareUrl = `http://pinterest.com/pin/create/button/?url=${url}
-                                                          &media=${imageUrl}   
-                                                          &description=${title}`;
+    let shareUrl = `http://pinterest.com/pin/create/button/?url=${encodeURIComponent(url)}
+                                                                      &media=${imageUrl}   
+                                                                      &description=${title}`;
     window.open(shareUrl)
   }
 
